498244f6e4f180a7d6adc5b62da8d3608caae37e
Daniel Jahnsmüller <mail@tastaturberuf.de>
2021-02-03 14:30:43 +0100

    merge language array with default translation

--- src/Resources/contao/languages/de/tl_form.php
+++ src/Resources/contao/languages/de/tl_form.php
@@ -1,16 +1,18 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_form'] =
-[
-    'form_content_legend' => 'Form Content Bundle',
-    'fc_show_content' => ['Zeige Content Elemente nach dem Absenden an', 'Wenn aktiviert, werden nach dem Absenden des Formular die hinterlegten Content Element statt des Formulars angezeigt.'],
-    'fc_mode' => ['Modus', 'Wählen Sie einen Modus aus.'],
-
-    'fc_mode_options' =>
+$GLOBALS['TL_LANG']['tl_form'] = array_merge(
+    $GLOBALS['TL_LANG']['tl_form'],
     [
-        'onetime' => 'einmalig',
-        'session' => 'Ganze Sitzung'
-    ],
+        'form_content_legend' => 'Form Content Bundle',
+        'fc_show_content' => ['Zeige Content Elemente nach dem Absenden an', 'Wenn aktiviert, werden nach dem Absenden des Formular die hinterlegten Content Element statt des Formulars angezeigt.'],
+        'fc_mode' => ['Modus', 'Wählen Sie einen Modus aus.'],
 
-    'edit_content' => 'Bearbeite Inhalts-Elemente für ID %s'
-];
+        'fc_mode_options' =>
+        [
+            'onetime' => 'einmalig',
+            'session' => 'Ganze Sitzung'
+        ],
+
+        'edit_content' => 'Bearbeite Inhalts-Elemente für ID %s'
+    ]
+);
--- src/Resources/contao/languages/en/tl_form.php
+++ src/Resources/contao/languages/en/tl_form.php
@@ -1,16 +1,18 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_form'] =
-[
-    'form_content_legend' => 'Form Content Bundle',
-    'fc_show_content' => ['Show content elements after submitting', 'If activated, after submitting the form the stored content elements are displayed instead of the form itself.'],
-    'fc_mode' => ['Mode', 'Select a mode.'],
-
-    'fc_mode_options' =>
+$GLOBALS['TL_LANG']['tl_form'] = array_merge(
+    $GLOBALS['TL_LANG']['tl_form'],
     [
-        'onetime' => 'one time',
-        'session' => 'whole session'
-    ],
+        'form_content_legend' => 'Form Content Bundle',
+        'fc_show_content' => ['Show content elements after submitting', 'If activated, after submitting the form the stored content elements are displayed instead of the form itself.'],
+        'fc_mode' => ['Mode', 'Select a mode.'],
 
-    'edit_content' => 'Edit content elements for ID %s'
-];
+        'fc_mode_options' =>
+        [
+            'onetime' => 'one time',
+            'session' => 'whole session'
+        ],
+
+        'edit_content' => 'Edit content elements for ID %s'
+    ]
+);

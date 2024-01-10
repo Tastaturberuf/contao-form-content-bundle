938abefa29367125f821119254c2c650d78a4c16
Tastaturberuf <mail@tastaturberuf.de>
2024-01-10 15:50:07 +0100

    Replace array_merge with array_replace

--- src/Resources/contao/languages/de/tl_form.php
+++ src/Resources/contao/languages/de/tl_form.php
@@ -1,8 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_form'] = array_merge(
-    $GLOBALS['TL_LANG']['tl_form'],
-    [
+$GLOBALS['TL_LANG']['tl_form'] = array_replace($GLOBALS['TL_LANG']['tl_form'] ?? [], [
         'form_content_legend' => 'Form Content Bundle',
         'fc_show_content' => ['Zeige Content Elemente nach dem Absenden an', 'Wenn aktiviert, werden nach dem Absenden des Formular die hinterlegten Content Element statt des Formulars angezeigt.'],
         'fc_mode' => ['Modus', 'Wählen Sie einen Modus aus.'],
--- src/Resources/contao/languages/en/tl_form.php
+++ src/Resources/contao/languages/en/tl_form.php
@@ -1,8 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_form'] = array_merge(
-    $GLOBALS['TL_LANG']['tl_form'],
-    [
+$GLOBALS['TL_LANG']['tl_form'] = array_replace($GLOBALS['TL_LANG']['tl_form'] ?? [], [
         'form_content_legend' => 'Form Content Bundle',
         'fc_show_content' => ['Show content elements after submitting', 'If activated, after submitting the form the stored content elements are displayed instead of the form itself.'],
         'fc_mode' => ['Mode', 'Select a mode.'],
